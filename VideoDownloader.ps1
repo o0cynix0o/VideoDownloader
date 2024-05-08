c0095f110c048cecb5c6f1086544d450de76c036
o0cynix0o <o0cynix0o@gmail.com>
2024-05-07 19:10:42 -0600

    Update VideoDownloader.ps1

--- VideoDownloader.ps1
+++ VideoDownloader.ps1
@@ -1,4 +1,14 @@
-$URL = Read-Host "Please enter the Videos URL"
-$Title = Read-Host "Please enter the Videos Title"
-.\yt-dlp $URL -P "G:\Downloads\YTDL\Temp" -o $Title
+#Use PATHTOOUTPUTDIR to customize script. 
+
+#Creat Variables 
+
+$URL = Read-Host "Please enter the Videos URL."
+
+$Title = Read-Host "Please enter the Videos Title."
+
+#yt-dlp command to download video 
+
+.\yt-dlp $URL -P "PATHTOOUTPUTDIR" -o $Title
+
+#Close window. 
 exit
