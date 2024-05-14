cca552291044ddb71f1172e29fb0868534c99807
o0cynix0o <o0cynix0o@gmail.com>
2024-05-14 15:23:46 -0600

    added some stuff

--- VideoDownloader.ps1
+++ VideoDownloader.ps1
@@ -8,7 +8,7 @@ $Title = Read-Host "Please enter the Videos Title."
 
 #yt-dlp command to download video 
 
-.\yt-dlp $URL -P "PATHTOOUTPUTDIR" -o $Title
+yt-dlp $URL -P "PATHTOOUTPUTDIR" -o $Title
 
 #Close window. 
 exit
